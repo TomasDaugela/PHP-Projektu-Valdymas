234b4dd76075ba759dae4c0fc7db5d25d6ff0bbb
Tomas <daugeltomas@gmail.com>
2020-08-06 10:10:42 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projektu Valdymas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+ require_once 'process.php';
+?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE crud";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "crud";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE data (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+course VARCHAR(30) NOT NULL
+
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+<?php
+    
+
+  $sql = "SELECT id, name, course FROM data";
+    $result = mysqli_query($mysqli, $sql);
+
+    print "<table>";
+    print "<th>Vardas</th><th>Kursas</th><th>Veiksmai</th>";    
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            print "<tr>";
+            echo "<td>" . $row["name"]. "</td>";
+            echo "<td>" . $row["course"]. "</td>";
+            echo "<td> <a href='index.php?edit=".$row["id"]."'> Edit </a>
+            <a class='red' href='index.php?delete=".$row["id"]."'>    Delete </a>
+            </td>";
+            
+        }
+        print "</tr>";
+    }
+    
+    print "</table>";
+    
+    mysqli_close($mysqli);
+  
+?>
+
+<form action="process.php" method="POST">
+    <input type="hidden" name="id" value="<?php echo $id; ?>">
+    <label>Vardas</label>
+    <input type="text" name="name" value="<?php echo $name; ?>" placeholder="Įveskite vardą"><br>
+    <label>Kursas</label>
+    <input type="text" name="course"value="<?php echo $course; ?>" placeholder="Įveskite kursą"><br>  
+    <?php
+    if($update==true):
+?>
+    <button class="update" type="submit" name="update">Atnaujinti</button>
+    <?php else: ?>
+        <button type="submit" name="save">Sukurti</button>
+    <?php endif; ?>
+</form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+
+    $mysqli = new mysqli('localhost','root','mysql','crud')
+    or die (mysqli_error($mysqli));
+
+    $id = 0;
+    $update = false;
+    $name = '';
+    $course = '';
+
+    if(isset($_POST['save'])){
+        $name = $_POST['name'];
+        $course = $_POST['course'];
+
+        $mysqli->query("INSERT INTO data (name, course) VALUES('$name','$course')") or
+        die($mysqli->error);
+
+        header("location: index.php");
+    }
+
+    if(isset($_GET['delete'])){
+        $id = $_GET['delete'];
+        $mysqli->query("DELETE FROM data WHERE id=$id") or
+         die($mysqli->error);
+        header("location: index.php");
+    }
+
+    if(isset($_GET['edit'])){
+        $id = $_GET['edit'];
+        $update = true;
+        $result= $mysqli->query("SELECT * FROM data WHERE id=$id") or
+         die($mysqli->error);
+
+             $row = $result->fetch_array();
+             $name = $row['name'];
+             $course = $row['course'];
+
+    }
+
+    if(isset($_POST['update'])){
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $course = $_POST['course'];
+
+        $mysqli->query("UPDATE data SET name='$name', course='$course' WHERE id=$id ") or
+        die($mysqli->error);
+
+        header("location: index.php");
+    }
+   
+    
+?>
